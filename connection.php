7c0783bdb14b3e697f9b305021e41c8fcecd9418
FLedeux <44844255+FLedeux@users.noreply.github.com>
2019-10-26 22:04:10 +0200

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+function connection(){
+  try {
+    return $objPdo = new PDO('mysql:host=?;port=3306;dbname=?','identifiant','mot de passe'); //a compléter
+  catch(Exception $exception){
+    session_start();
+    $_SESSION['BDD_ERROR']=$exception->getMessage();
+
+    $host  = $_SERVER['HTTP_HOST'];
+    $uri   = rtrim(dirname($_SERVER['PHP_SELF']), '/\\');
+    $extra = 'error_bdd.php';
+
+    header("Location: http://$host$uri/$extra");
+    exit();
+  }
+}
+?>
